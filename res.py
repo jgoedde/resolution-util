17fb08723a54d94ea7c582aa380508fa3046d894
Julian Gödde <goedde@tuta.io>
2023-07-08 18:08:33 +0200

    Add functionality to change screen resolution
    A new file 'res.py' has been added. This file includes a method 'switch_resolution' to change the screen resolution using the NVIDIA Control Panel. This is achieved by adding necessary libraries and creating an argument parser for the resolution argument. This new functionality allows for dynamically switching screen resolution based on user needs.

---
+++ res.py
@@ -0,0 +1,44 @@
+﻿import argparse
+import time
+import pywinauto
+import pyautogui
+
+
+def switch_resolution(resolution):
+    # Open NVIDIA Control Panel
+    pyautogui.hotkey('win', 's')
+    pyautogui.typewrite('NVIDIA Control Panel')
+    time.sleep(0.5)
+    pyautogui.press('enter')
+    time.sleep(3)
+
+    # Find the NVIDIA Control Panel window
+    app = pywinauto.Desktop(backend='uia').window(title='NVIDIA Systemsteuerung')
+    app.set_focus()
+
+    # Select "Change resolution"
+    app.child_window(title='Auflösung ändern', found_index=0).click_input()
+
+    # Find and click the matching resolution
+    resolution_item = app.child_window(title_re=f'.*{resolution}.*', found_index=0)
+    resolution_item.click_input()
+
+    # Click "Apply"
+    app.child_window(title='Übernehmen').click_input()
+
+    # Close the NVIDIA Control Panel
+    app.close()
+
+
+if __name__ == "__main__":
+    # Create the argument parser
+    parser = argparse.ArgumentParser(description='Switch resolution using NVIDIA Control Panel.')
+
+    # Add the resolution argument
+    parser.add_argument('resolution', choices=['1920', '1440'], help='Resolution to switch to (1920 or 1440)')
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Call the function to switch the resolution
+    switch_resolution(args.resolution)
